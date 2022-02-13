5abe2032fc1c96bca6532afd70bea54175cb77d4
Kintoho <hohlov.aleksandr.a@gmail.com>
2022-02-14 00:20:29 +0400

    7 задание

---
+++ src/main/java/stage1/Bottles.java
@@ -0,0 +1,11 @@
+package stage1;
+
+public class Bottles {
+
+    public static void main(String[] args){
+        for(int i = 100; i>0;i--){
+            System.out.println(i + " бутылок стояло на столе, одна из них упала и разбилась.");
+        }
+        System.out.println("А всё! Больше бить нечего!!!");
+    }
+}
